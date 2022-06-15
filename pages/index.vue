cd57de21872fd7a69bdeb7082dabe0d9ddfb734a
Igor <ilg2005@mail.ru>
2022-06-15 12:42:53 +0300

    pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Navbar/>
-    <div class="mt-5 text-center">
+    <div class="container mt-5 text-center">
       <h1>Welcome to Restaurantly!</h1>
       <a href="/restaurants">Go to restaurants?</a>
     </div>
---
+++ pages/restaurants.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Navbar/>
+    <div class="container mt-5 text-center">
+      <h1>Restaurants</h1>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
