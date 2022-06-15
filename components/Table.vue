46df9f60d7b959695240667f04112f20c45ee926
Igor <ilg2005@mail.ru>
2022-06-15 15:56:55 +0300

    restaurants->table

---
+++ components/Table.vue
@@ -0,0 +1,29 @@
+<template>
+  <table class="table table-hover table-bordered mt-5 table-striped">
+    <thead class="table-light">
+    <tr>
+      <th scope="col">Rank</th>
+      <th scope="col">Chain</th>
+    </tr>
+    </thead>
+    <tbody class="table-group-divider">
+    <tr v-for="restaurant in restaurants"
+        :key="restaurant.id"
+    >
+      <th scope="row">{{ restaurant.rank }}</th>
+      <td >{{ restaurant.name }}</td>
+    </tr>
+    </tbody>
+  </table>
+
+</template>
+
+<script setup>
+defineProps(['restaurants']);
+</script>
+
+<style scoped>
+.col {
+  margin-right: 5rem;
+}
+</style>
--- pages/restaurants.vue
+++ pages/restaurants.vue
@@ -2,11 +2,18 @@
   <div>
     <Navbar/>
     <div class="container mt-5 text-center">
-      <h1>Restaurants</h1>
+      <h1>Top 50: The Ranking</h1>
+        <div class="row">
+          <Table class="col" :restaurants="data1"/>
+          <Table class="col" :restaurants="data2"/>
+        </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import data from '@/data.json';
 
+const data1 = [...data].splice(0, data.length / 2);
+const data2 = [...data].splice(data.length / 2, data.length);
 </script>
