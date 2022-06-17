232ce407106ac910d6d71ce01f6212fd9be338be
Igor <ilg2005@mail.ru>
2022-06-17 12:26:24 +0300

    restaurant

--- pages/restaurants/[restaurant].vue
+++ pages/restaurants/[restaurant].vue
@@ -1,16 +1,82 @@
 <template>
   <div>
     <Navbar/>
-    <div class="container mt-5 text-center">
-      <h1 class="mt-10">Reataurant Page</h1>
-      <img src="#" alt="">
+    <div class="restaurant-container mt-10">
+      <div class="image-container">
+        <img :src="restaurant.imageUrl" alt="Restaurant picture" />
+      </div>
+      <div class="info-container">
+        <h1>{{ restaurantName }}</h1>
+        <div class="stats-container">
+          <h5>Revenue (in billions)</h5>
+          <p>${{ restaurant.revenue }}</p>
+        </div>
+        <div class="stats-container">
+          <h5>Number of Stores</h5>
+          <p>{{ restaurant.numberOfStores }}</p>
+        </div>
+        <p class="content">{{ restaurant.content }}</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import restaurants from "@/data.json";
+
+const route = useRoute();
+const restaurantName = route.params.restaurant;
+
+const restaurant = restaurants.filter(r => r.name === restaurantName)[0];
 </script>
 
 <style scoped>
+.restaurant-container {
+  display: flex;
+  align-items: center;
+}
+.image-container {
+  width: 75%;
+  height: 95vh;
+}
 
+.image-container img {
+  width: 100%;
+  height: 100%;
+}
+.restaurant-not-found {
+  height: 75vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.info-container {
+  padding: 3rem;
+  width: 50%;
+}
+.info-container h1 {
+  text-transform: uppercase;
+  font-size: 6rem;
+  margin-bottom: 3rem;
+}
+.stats-container {
+  display: flex;
+  align-items: flex-end;
+  margin-bottom: 1rem;
+}
+.stats-container h5 {
+  width: 20rem;
+  font-size: 2rem;
+  margin: 0;
+  margin-right: 5rem;
+}
+.stats-container p {
+  font-size: 2rem;
+  margin: 0;
+}
+.content {
+  font-size: 1.5rem;
+  margin-top: 4rem;
+}
 </style>
